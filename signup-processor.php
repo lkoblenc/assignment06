223686b1139ff62d1c07c703ed72f6f1caf03ee5
salam1996 <47185631+salam1996@users.noreply.github.com>
2019-04-03 12:48:06 -0400

    Final form processor

--- signup-processor.php
+++ signup-processor.php
@@ -15,26 +15,15 @@
 	// 2. Perform database query
 	$query  = "INSERT INTO members (firstname, lastname, email) VALUES ('$firstname','$lastname','$email')";
 	$result = mysqli_query($connection, $query);
-
-?>
-
-<?php
 	if ($result) {
 		header("Location: about.php"); /* Redirect browser */
-  exit();
-?>
-
-<?php
+  	exit();
 	} else {
 		die("Database query failed.");
 	}
-?>
-
-<?php
 	// 4. Step 4 is unnecessary here because we didn't 
 	//	  get data that needs to be released
 	//mysqli_free_result($result);
-
     mysqli_free_result($result);
 	// 5. Close database connection
 	mysqli_close($connection);
